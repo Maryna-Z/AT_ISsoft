62ab70f837b6184dca2fb2f3c412ec37d704f4cd
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-03-25 00:12:55 +0300

    parsing xml is came back

--- OnlineStore/consoleApp/src/main/java/com/marina/StoreApp.java
+++ OnlineStore/consoleApp/src/main/java/com/marina/StoreApp.java
@@ -21,22 +21,24 @@ public class StoreApp {
         ProductPublisher publisher = new Store().getPublisher(publisherType);
         List<Product> products = publisher.getProducts();
         Store store = new Store(products);
-        int selectedProductId1 = Utils.getIntFromConsole(System.in);
-        store.createOrder(selectedProductId1);
-        int selectedProductId2 = Utils.getIntFromConsole(System.in);
-        store.createOrder(selectedProductId2);
-        store.clearBothProduct();
-        
+
         URL resource = ClassLoader.getSystemResource("sort.xml");
         DOMExecuter executer = new DOMExecuter();
         Map<String, String> sortCondition = executer.XMLreader(resource.getPath());
-
         List<Comparator<Product>> comparatorList = new ListProductComparators(sortCondition).getComparatorList(sortCondition);
         products.sort(new ChainComparator(comparatorList));
         System.out.println("\n sorting ********************************\n");
         for (int i = 0; i < 5; i++) {
             System.out.println(products.get(i));
         }
+
+        int selectedProductId1 = Utils.getIntFromConsole(System.in);
+        store.createOrder(selectedProductId1);
+        int selectedProductId2 = Utils.getIntFromConsole(System.in);
+        store.createOrder(selectedProductId2);
+        store.clearBothProduct();
+
+
     }
 
 }
