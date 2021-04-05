44e1791fa85a129b8396312d60e066155d4d6c1b
Maryna Zahorskaya <zagorskayam@mail.ru>
2021-04-05 16:26:53 +0300

    delete check method

--- OnlineStore/store/src/main/java/com/marina/services/http/CommonHandler.java
+++ OnlineStore/store/src/main/java/com/marina/services/http/CommonHandler.java
@@ -3,16 +3,11 @@ package com.marina.services.http;
 import com.marina.exception.CommonException;
 import com.marina.utility.Utils;
 import com.sun.net.httpserver.HttpExchange;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Base64;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
-
-import static com.marina.constants.Constants.KEY;
 
 public abstract class CommonHandler {
     protected void createResponse(HttpExchange exchange, int responseStatus, Object object) {
@@ -36,21 +31,5 @@ public abstract class CommonHandler {
         } catch (IOException ex) {
             throw new  CommonException("Error to generate response", ex);
         }
-    }
-
-    protected boolean checkToken(HttpExchange httpExchange){
-        List<String> authorizationList = httpExchange.getRequestHeaders()
-                .get("Authorization");
-        if (authorizationList == null){
-            return false;
-        }
-        String authorization = authorizationList.stream().findFirst().orElse(null);
-
-        if(StringUtils.isBlank(authorization)){
-            return false;
-        }
-        authorization = authorization.replace("Bear:", StringUtils.EMPTY).trim();
-        authorization = new String(Base64.getDecoder().decode(authorization));
-        return KEY.equals(authorization);
     }
 }
